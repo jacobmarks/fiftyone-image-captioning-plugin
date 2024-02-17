53f59f3d7b525d55813bacdfb8c95f493d1d7868
Jacob Marks <jamarks13@gmail.com>
2024-02-16 18:09:39 -0800

    cleanup

--- __init__.py
+++ __init__.py
@@ -73,54 +73,6 @@ def _execution_mode(ctx, inputs):
         )
 
 
-# def _get_target_view(ctx, target):
-#     if target == "SELECTED_SAMPLES":
-#         return ctx.view.select(ctx.selected)
-
-#     if target == "DATASET":
-#         return ctx.dataset
-
-#     return ctx.view
-
-
-# def _list_target_views(ctx, inputs):
-#     has_view = ctx.view != ctx.dataset.view()
-#     has_selected = bool(ctx.selected)
-#     default_target = "DATASET"
-#     if has_view or has_selected:
-#         target_choices = types.RadioGroup()
-#         target_choices.add_choice(
-#             "DATASET",
-#             label="Entire dataset",
-#             description="Run model on the entire dataset",
-#         )
-
-#         if has_view:
-#             target_choices.add_choice(
-#                 "CURRENT_VIEW",
-#                 label="Current view",
-#                 description="Run model on the current view",
-#             )
-#             default_target = "CURRENT_VIEW"
-
-#         if has_selected:
-#             target_choices.add_choice(
-#                 "SELECTED_SAMPLES",
-#                 label="Selected samples",
-#                 description="Run model on the selected samples",
-#             )
-#             default_target = "SELECTED_SAMPLES"
-
-#         inputs.enum(
-#             "target",
-#             target_choices.values(),
-#             default=default_target,
-#             view=target_choices,
-#         )
-#     else:
-#         ctx.params["target"] = "DATASET"
-
-
 def run_qwen_vl_chat(sample):
     filepath = get_filepath(sample)
     return replicate.run(
